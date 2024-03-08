ad8694a2d96a764c931ac50c9a15c529de92347c
NagaAbishek <95166766+NagaAbishek@users.noreply.github.com>
2024-03-08 15:21:45 +0530

    Update test_batch.py

--- test_batch.py
+++ test_batch.py
@@ -1,1 +1,12 @@
+import boto3
 
+def upload_file(file_name, bucket, object_name=None):
+    s3_client = boto3.client('s3')
+    if object_name is None:
+        object_name = file_name
+    s3_client.upload_file(file_name, bucket, object_name)
+    print(f"Uploaded {file_name} to {bucket}/{object_name}")
+
+if __name__ == '__main__':
+    # This uploads 'output.txt' to the 'data' folder within 'example-s3-bucket'
+    upload_file('output.txt', 'automate-github-action', 'data/output.txt')  # Modify 'example-s3-bucket'
