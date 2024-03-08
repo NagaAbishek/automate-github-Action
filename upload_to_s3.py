2f3ccffa31fc6042162752b37c25c807ad579364
NagaAbishek <95166766+NagaAbishek@users.noreply.github.com>
2024-03-08 15:01:02 +0530

    Update upload_to_s3.py

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -1,4 +1,3 @@
-# upload_to_s3.py
 import boto3
 
 def upload_file(file_name, bucket, object_name=None):
@@ -9,4 +8,6 @@ def upload_file(file_name, bucket, object_name=None):
     print(f"Uploaded {file_name} to {bucket}/{object_name}")
 
 if __name__ == '__main__':
-    upload_file('output.txt', 'example-s3-bucket', 'output.txt')  # Replace 'example-s3-bucket'
+    # This uploads 'output.txt' to the 'data' folder within 'example-s3-bucket'
+    upload_file('output.txt', 'automate-github-action', 'data/output.txt')  # Modify 'example-s3-bucket'
+
